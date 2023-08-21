ab5e283d38978ee45fb6473ece80cf3952c2d8d4
soeurwiki <atia.safiya@gmail.com>
2023-08-22 01:18:43 +0200

    reorganizing files

--- Airelle_rnaseq/Analysis/run_on_cluster/get_matrix/get_count_mtx.sh
+++
@@ -1,5 +0,0 @@
-module load system/R-4.2.3_Miniconda3
-
-sample=$(ls /work/project/LeCam_U1194/AIRELLE/RNASEQ/nextflow_full/star_salmon/featurecounts | grep .featureCounts.txt$ | sed 's/.featureCounts.txt//g')
-
-python count_mtx.py  ${sample}
